786a67b0af3ed31ea86abcbe4e7d70fea8b802be
Neil <akashneil@live.com>
2021-12-04 00:56:00 +0400

    -

---
+++ src/main/java/io/github/seebaware/App.java
@@ -0,0 +1,13 @@
+package io.github.seebaware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
