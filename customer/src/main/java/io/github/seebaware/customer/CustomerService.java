83e4196aaf422130a4fc063be255e8083e565e23
Neil <akashneil@live.com>
2021-12-04 16:58:48 +0400

    -

--- customer/src/main/java/io/github/seebaware/customer/CustomerService.java
+++ customer/src/main/java/io/github/seebaware/customer/CustomerService.java
@@ -3,12 +3,15 @@ package io.github.seebaware.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepo customerRepo) {
     public void register(CustomerRegRequest customerRegRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegRequest.firstName())
                 .lastName(customerRegRequest.lastName())
                 .email(customerRegRequest.email())
                 .build();
+
+        customerRepo.save(customer);
+
     }
 }
