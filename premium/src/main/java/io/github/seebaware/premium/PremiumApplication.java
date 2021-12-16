d3da40fbdff3eeb60ceed8c8fecddd21f2027d9f
- <->
2021-12-16 23:49:21 +0400

    eureka

--- premium/src/main/java/io/github/seebaware/premium/PremiumApplication.java
+++ premium/src/main/java/io/github/seebaware/premium/PremiumApplication.java
@@ -2,8 +2,10 @@ package io.github.seebaware.premium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PremiumApplication {
 
     public static void main(String[] args) {
